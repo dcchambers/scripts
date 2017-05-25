cf2e99bb58a98d7b9b3cc75ef0b1fb895cdcf29e
Dakota Chambers <dakotachambers@gmail.com>
2017-05-25 13:40:35 -0500

    Add a script that converts du output to a CSV file

---
+++ convert-du-output-to-csv.py
@@ -0,0 +1,29 @@
+# Reads in the output from $ du -csh --time * >> output.log
+# and prints it to a CSV file in the format "date,user,sizeOfHomeDir"
+#
+# Usage: Run $ python convert-to-csv.py [arg1]
+# where [arg1] is the output.log file to be converted to CSV.
+#
+
+import os
+import sys
+
+if len(sys.argv) > 3:
+    print 'Too many command line arguments. Expected: 2 [input file] [output file].'
+    sys.exit()
+elif len(sys.argv) < 3:
+    print 'Too few command line arguments. Expected: 2 [input file] [output file].'
+    sys.exit()
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+f2 = open(outputFile,'w+')
+f2.write('date,user,size-of-homedir\n')
+
+with open(inputFile,'r') as f1:
+    for line in f1:
+        tempArray = []
+        tempArray = line.split('\t')
+        # print tempArray #DEBUG
+        f2.write(tempArray[1]+','+tempArray[2].rstrip('\n')+','+tempArray[0]+'\n')
